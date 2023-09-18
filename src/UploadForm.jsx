11b1e7f956fd54efca7449d6712b0e14bd4618de
phil <phil.roth077@gmail.com>
2023-09-18 14:06:12 -0700

    upload things

--- src/UploadForm.jsx
+++ src/UploadForm.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 const UploadForm = () => {
 	const [file, setFile] = useState(null)
+	const [name, setName] = useState("")
 	const [errors, setErrors] = useState([])
 
 	const onAddFile = (e) => {
@@ -23,6 +24,7 @@ const UploadForm = () => {
 		setErrors([])
 		const formData = new FormData()
 		formData.append("file", file)
+		formData.append("playlist_name", name)
 		console.log({ file, formData })
 		// debugger
 		fetch("http://127.0.0.1:5555/upload", {
@@ -39,14 +41,21 @@ const UploadForm = () => {
 	}
 	return (
 		<form onSubmit={handleSubmit}>
+			{errors.length > 0
+				? errors.map((er) => <p style={{ color: "red" }}>{er}</p>)
+				: null}
 			<label>
-				{errors.length > 0
-					? errors.map((er) => <p style={{ color: "red" }}>{er}</p>)
-					: null}
 				Import your CSV/TXT Setlist <br />
+				<input
+					type="text"
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Playlist Name"
+					value={name}
+					name="name"
+				/>
 				<input type="file" onChange={onAddFile} accept=".txt,.csv" />
-				<input type="submit" value="Upload" />
 			</label>
+			<input type="submit" value="Upload" />
 		</form>
 	)
 }
